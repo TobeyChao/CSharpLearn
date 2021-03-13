489d8010825cf25b0200430c19d159a2045ae40a
TobeyChao <tobeychao@qq.com>
2021-03-13 11:30:48 +0800

    DateTimeTest

---
+++ TestDateTime.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Threading;
+
+namespace C_Learn
+{
+    class Program
+    {
+        /// <summary>
+        /// 将dateTime格式转换为Unix时间戳
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static int DateTimeToUnixTime(DateTime dateTime)
+        {
+            return (int)(dateTime - TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+
+        /// <summary>
+        /// 将Unix时间戳转换为dateTime格式
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static DateTime UnixTimeToDateTime(int time)
+        {
+            if (time < 0)
+                throw new ArgumentOutOfRangeException("time is out of range");
+
+            return TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1)).AddSeconds(time);
+        }
+
+        public static string UnixTimeToDateTime(int time, string timestr)
+        {
+            if (time < 0)
+                throw new ArgumentOutOfRangeException("time is out of range");
+
+            return TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1)).AddSeconds(time).ToString(timestr);
+        }
+
+        public static bool IsSameDay(int timeLeft, int timeRight)
+        {
+            DateTime left = UnixTimeToDateTime(timeLeft);
+            DateTime right = UnixTimeToDateTime(timeRight);
+            return left.Date == right.Date;
+        }
+        static void Main(string[] args)
+        {
+            var date1 = DateTime.Now;
+            var unix1 = DateTimeToUnixTime(date1);
+            Thread.Sleep(500);
+            var date2 = DateTime.Now;
+            var unix2 = DateTimeToUnixTime(date2);
+            var isSameDay1 = date1 == date2;
+            //DateTime.Date不带时间，00:00:00
+            //所以可以用来比较是不是同一天
+            var isSameDay2 = date1.Date == date2.Date;
+            var dayStr1 = date1.Date.ToShortDateString();
+            var dayStr2 = date1.Date.ToLongDateString();
+            var dayStr3 = date1.Date.ToShortTimeString();
+            var dayStr4 = date1.Date.ToLongTimeString();
+            var dayInt1 = date1.Date.Second;
+            var dayInt2 = date2.Date.Second;
+            Console.WriteLine("Hello");
+        }
+    }
+}
